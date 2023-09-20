713c182c5322ef891d2d33a7a19e31ab791896bf
suppi39 <81616158+suppi39@users.noreply.github.com>
2023-09-20 16:47:12 +0530

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,19 @@
+let canvas= document.getElementById("signature-pad");
+
+function resizedCanvas(){
+    let ratio =Math.max(window.devicePixelRatio || 1,1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio,ratio);
+
+}
+window.onresize =resizeCanvas;
+resizeCanvas();
+
+let signaturePad = new SignaturePad(canvas,{
+    backgroundColor : "rgb(250,250,250)"
+});
+
+document.getElementById("clear").addEventListener("click",function (){
+    signaturePad.clear();
+});
